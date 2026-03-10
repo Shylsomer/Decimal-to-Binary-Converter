cb2c77b325ff3a32ee027316d87b5d425633215b
Shylsomer <pulmonoscorpius70@gmail.com>
2026-03-10 15:58:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+"""
+ Decimal to Binary Converter
+
+ Description: Takes the users number and converts it into binary.
+ Continues doing that until the program is stopped or 'E' is entered.
+
+ Author: Torin
+ Date: March 3rd, 2026
+"""
+
+from sys import exit
+
+def greeting():
+    """
+    Greets user and displays
+    program overview.
+    """
+    print("***** DECIMAL TO BINARY CONVERTER *****\n"
+          "This program converts numbers into binary.\n"
+          "Insert your number at the prompt below and\n"
+          "it will be converted. Enter 'E' to exit.")
+
+greeting.called = False
+
+def main():
+    """
+    Keeps asking the user for input
+    until 'E' is pressed. Converts
+    input into binary and displays
+    the result.
+    """
+    if not greeting.called:
+        greeting()
+
+    num = ""
+
+    while num not in ["E", "e"]:
+        bin = []
+
+        try:
+            num = int(input("\nEnter your number: "))
+
+        except ValueError:
+            if num not in ["E", "e"]:
+                exit()
+            else:
+                exit("ERROR: Input must be a number.")
+
+        if num == 0:
+            bin.insert(0, 0)
+
+        while num > 0:
+            if num % 2 == 1:
+                bin.insert(0, 1)
+            else:
+                bin.insert(0, 0)
+            num = num // 2
+
+        print("Your binary number: ", end='')
+        print(*bin, sep='')
+
+if __name__ == "__main__":
+    main()
